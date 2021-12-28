f2bc24733d8f866ef7a0db408efbbedcd39683ab
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2021-12-28 16:08:17 -0500

    Adding `butterband` Julia function

--- butter2sos.jl
+++ butter2sos.jl
@@ -39,6 +39,44 @@ function butter2sos(order::Integer, Fc::Real, Fs::Real; type::String="lowpass")
     SecondOrderSections(Vb, k)
 end
         
+"""
+    sos = butterband(order, Fl, Fh, Fs, type="bandpass")
+
+Z-domain IIR Butterworth design of Bandpass/Bandstop filters.
+`Fl` and `Fh` indicate the lower and upper natural frequency edges,
+with `Fs` indicating the discrete sample rate, and `order` specifies
+the cascaded Biquads filter order.
+"""
+function butterband(order::Integer, Fl::Real, Fh::Real, Fs::Real; type::String="bandpass")
+    (Fs > 0) || error("Fs sampling rate must be greater than 0 Hz (DC).")
+    (order > 0) || error("Filter order must be greater than zero.")
+    (type ∈ ["bandpass", "bandstop"]) || error("type must indicate 'bandpass' or 'bandstop'.")
+    if (Fl > Fh)
+        Fl, Fh = Fh, Fl
+    end
+    ((Fl > 0 && Fl < Fs/2) && (Fh > 0 && Fh < Fs/2)) || error("Upper/lower bounds must be in range (0, Fs/2].")
+    Nstages = ceil(Int, order/2)
+    Ncoeffs = Nstages * 6
+    matptr = Vector{Float32}(undef, Ncoeffs)
+    if (type == "bandpass")
+        ftype = Int(0)
+    else
+        ftype = Int(1)
+    end
+    ccall((:jl_butterband, "butterlib"), Cvoid, (Cint, Cfloat, Cfloat, Cfloat, Cint, Ptr{Float32}), order, Fl, Fh, Fs, ftype, matptr)
+    sosmat = Matrix{Float32}(transpose(reshape(matptr, (Nstages, 6))))
+    Vb = Vector{Biquad{:z, Float32}}(undef, Nstages)
+    K, kstg = 1
+    for iStg = 1:Nstages
+        kstg = sosmat[iStg,1]
+        K *= kstg
+        sosmat[iStg,1:3] ./= kstg
+        Vb[iStg] = Biquad{:z, Float32}(sosmat[iStg,1], sosmat[iStg,2], sosmat[iStg,3], sosmat[iStg,5], sosmat[iStg,6])
+    end
+    SecondOrderSections(Vb,K)
+end
+    
+
 
 
     
