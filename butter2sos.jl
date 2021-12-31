7c376429967d64e96f5a71e3f30fcf5397d7853a
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2021-12-30 22:30:04 -0500

    Switching to symbols. Fixing reshape.

--- butter2sos.jl
+++ butter2sos.jl
@@ -1,38 +1,31 @@
 using DSP
 
 """
-    sos = butter2sos(order, Fc, Fs, type="lowpass")
+    sos = butter2sos(order, Fc, Fs, type=:lowpass)
 
 Z-domain IIR Butterworth design of Lowpass/Highpass/Allpass filters.
 `Fc` indicates the corner "-3 dB" natural frequency point, whereas `Fs` indicates
 the discrete sample rate and `order` specifies the cascade filter order. The returned
 `sos` SecondOrderSection type is compatible with DSP.jl.
 """
-function butter2sos(order::Integer, Fc::Real, Fs::Real; type::String="lowpass")
+function butter2sos(order::Integer, Fc::Real, Fs::Real; type::Symbol=:lowpass)
     (Fc > 0 && Fs > 0) || error("Fc and/or Fs must be greater than 0 Hz (DC).")
     (Fc <= Fs/2) || error("Upperband `Fhi` cannot exceed Nyquist.")
-    (type ∈ ["lowpass", "highpass", "allpass"]) || error("type must indicate 'lowpass', 'highpass', or 'allpass.'")
-
+    (type ∈ [:lowpass, :highpass, :allpass]) || error("type must indicate ':lowpass', ':highpass', or ':allpass.'")
     Nstages = ceil(Int, order/2)
     Ncoeffs = Nstages * 6
     matptr = Vector{Float32}(undef, Ncoeffs)
-    
-    # based on input, discern filter type.
-    if (type == "lowpass")
+    if (type == :lowpass)
         ftype = Int(0)
-    elseif (type == "highpass")
+    elseif (type == :highpass)
         ftype = Int(1)
     else
         ftype = Int(2)
     end
-
-    # butterlib.dll has both butter and butterband available.
     ccall((:jl_butter, "butterlib"), Cvoid, (Cint, Cfloat, Cfloat, Cint, Ptr{Float32}), order, Fc, Fs, ftype, matptr)
-    sosmat = Matrix{Float32}(transpose(reshape(matptr, (Nstages, 6))))
+    sosmat = Matrix{Float32}(transpose(reshape(matptr, (6, Nstages))))
     Vb = Vector{Biquad{:z, Float32}}(undef, Nstages)
-
-    # normalize by leading coefficient.
-    K = prod(sosmat[:,1])
+    K = prod(sosmat[:,1]) # normalize by lead coefficient and extract gain, (whichever ordering.)
     kstg = 1
     for iStg = 1:Nstages
         kstg = sosmat[iStg,1]
@@ -43,17 +36,17 @@ function butter2sos(order::Integer, Fc::Real, Fs::Real; type::String="lowpass")
 end
         
 """
-    sos = butterband(order, Fl, Fh, Fs, type="bandpass")
+    sos = butterband(order, Fl, Fh, Fs, type=:bandpass)
 
 Z-domain IIR Butterworth design of Bandpass/Bandstop filters.
 `Fl` and `Fh` indicate the lower and upper natural frequency edges,
 with `Fs` indicating the discrete sample rate, and `order` specifies
 the cascaded Biquads filter order.
 """
-function butterband(order::Integer, Fl::Real, Fh::Real, Fs::Real; type::String="bandpass")
+function butterband(order::Integer, Fl::Real, Fh::Real, Fs::Real; type::Symbol=:bandpass)
     (Fs > 0) || error("Fs sampling rate must be greater than 0 Hz (DC).")
     (order > 0) || error("Filter order must be greater than zero.")
-    (type ∈ ["bandpass", "bandstop"]) || error("type must indicate 'bandpass' or 'bandstop'.")
+    (type ∈ [:bandpass, :bandstop]) || error("type must indicate ':bandpass' or ':bandstop'.")
     if (Fl > Fh)
         Fl, Fh = Fh, Fl
     end
@@ -61,13 +54,9 @@ function butterband(order::Integer, Fl::Real, Fh::Real, Fs::Real; type::String="
     Nstages = ceil(Int, order/2)
     Ncoeffs = Nstages * 6
     matptr = Vector{Float32}(undef, Ncoeffs)
-    if (type == "bandpass")
-        ftype = Int(0)
-    else
-        ftype = Int(1)
-    end
+    ftype = (type == :bandpass) ? Int(0) : Int(1)
     ccall((:jl_butterband, "butterlib"), Cvoid, (Cint, Cfloat, Cfloat, Cfloat, Cint, Ptr{Float32}), order, Fl, Fh, Fs, ftype, matptr)
-    sosmat = Matrix{Float32}(transpose(reshape(matptr, (Nstages, 6))))
+    sosmat = Matrix{Float32}(transpose(reshape(matptr, (6, Nstages))))
     Vb = Vector{Biquad{:z, Float32}}(undef, Nstages)
     kstg = 1
     K = prod(sosmat[:,1])
