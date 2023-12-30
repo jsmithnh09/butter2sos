deed7c14f3bb9f735d41b7692f70f4007a5ac183
jsmithnh09 <jsmithnh09@gmail.com>
2023-12-30 17:12:06 -0500

    Adding more exports

--- python/pybutter/src/pybutter/__init__.py
+++ python/pybutter/src/pybutter/__init__.py
@@ -1,4 +1,18 @@
-from .core import butter, butterband
+from .core import (
+    butter, 
+    butterband, 
+    pole2quad, 
+    stable, 
+    readsosbin, 
+    mksosbin
+)
 
 # only expose the designer methods.
-__all__ = ["butterband", "butter"]
+__all__ = [
+    "butterband", 
+    "butter", 
+    "pole2quad", 
+    "stable",
+    "readsosbin",
+    "mksosbin"
+]
