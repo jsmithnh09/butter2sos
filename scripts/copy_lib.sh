e782702cdae103e2ce2ae00a58708e5e12a65b6e
jsmithnh09 <jsmithnh09@gmail.com>
2024-01-07 16:46:01 -0500

    Adding file detect in the copy step.

--- scripts/copy_lib.sh
+++ scripts/copy_lib.sh
@@ -3,3 +3,7 @@
 # Copy the DLL to the python package for bundling and testing.
 # ===========================================================
 cp bazel-bin/lib/butterlib.dll src/pybutter/
+if [ ! -f src/pybutter/butterlib.dll ]; then
+    echo "DLL is missing.";
+    exit 1;
+fi
